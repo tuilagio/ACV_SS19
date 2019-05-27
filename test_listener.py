3a7357e94ecc9429fbf8bb7338d7293fdc12be6e
LeyendNk <lyndnktheone@gmail.com>
2019-05-27 15:16:24 +0200

    test_listener classifies in the receiving machine

--- test_listener.py
+++ test_listener.py
@@ -1,12 +1,24 @@
 import numpy as np
 import cv2
 from tcp_pickle_stream import listener
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
 
-
-listen = listener('localhost')
+listen = listener('192.168.1.3')
+model = ResNet50(weights='imagenet')
 while(True):
 
-    package = listen.get_frame()
+    img = listen.get_frame()
+    img = np.expand_dims(x, axis=0)
+    img = preprocess_input(x)
+
+    preds = model.predict(x)
+    predict_string = decode_predictions(preds, top=3)[0]
+    predict_string = ''.join(str(predict_string[0]))
+
+    cv2.putText(img, predict_string, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), lineType=cv2.LINE_AA)
+
     #if package:
         #cap = cv2.VideoCapture(0)
         # Capture frame-by-frame
@@ -16,7 +28,7 @@ while(True):
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Display the resulting frame
-    cv2.imshow('frame', package)
+    cv2.imshow('frame', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         listen.close()
         break
@@ -24,3 +36,4 @@ while(True):
 # When everything done, release the capture
 #cap.release()
 cv2.destroyAllWindows()
+
